e4a8e2d78c73884e6b3fd3cf547ebb15a99cd54c
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2024-10-08 09:37:12 +0530

    Refactor binary search tree implementation for improved performance and readability

--- unit 3/Binary tree/bst2.c
+++ unit 3/Binary tree/bst2.c
@@ -65,12 +65,56 @@ node *deNode(node *root, int x)
     return root;
 }
 
+void inOrder(node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+node *insert(node *root, int x)
+{
+    if (root == NULL)
+    {
+        node *temp = (node *)malloc(sizeof(node));
+        temp->data = x;
+        temp->left = temp->right = NULL;
+        return temp;
+    }
+
+    if (x < root->data)
+    {
+        root->left = insert(root->left, x);
+    }
+    else if (x > root->data)
+    {
+        root->right = insert(root->right, x);
+    }
+
+    return root;
+}
+
 int main()
 {
     int a, b, c;
     node *root = NULL;
-    node *temp = NULL;
-    node *sucessor = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+    inOrder(root);
+    printf("\n");
+    root = deNode(root, 20);
+    inOrder(root);
+    printf("\n");
+    root = deNode(root, 70);
+    inOrder(root);
 
     return 0;
 }
